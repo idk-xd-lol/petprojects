0ac966a682f21dd897344f0a4eb796f833207b71
idk-xd-lol <ya.xd.work.02@gmail.com>
2026-02-06 15:28:29 +0100

    created base-to-dec conventer

--- base_conventer/main.py
+++ base_conventer/main.py
@@ -1,20 +1,22 @@
-#getting decimal num
-decimal_num = None
-while not type(decimal_num) == int:
-    decimal_num = input("Enter decimal num:\t")
+#select mode
+mode = None
+while not type(mode) == int and not mode in range(0, 2):
+    mode = input("Enter mode(0 for dec-to-base, 1 for base-to-dec):\t")
     try:
-        decimal_num = int(decimal_num)
+        mode = int(mode)
     except:
-        decimal_num = None
+        mode = None
 
-#getting base num
+#getting base
 base = None
-while not type(base) == int:
+while not type(base) == int and not base in range(0, 16):
     base = input("Enter base(max: 16):\t")
     try:
         base = int(base)
     except:
         base = None
+
+LETTERS = ["A", "B", "C", "D", "E", "F"]
 
 #convert decimal to base
 def dec_to_base(decimal_num):
@@ -29,17 +31,52 @@ def dec_to_base(decimal_num):
         nums.append(num) #adds remainders to list
     return nums
 
-LETTERS = ["A", "B", "C", "D", "E", "F"] 
-num_list = dec_to_base(decimal_num)
-result = ""
-for i in range(len(num_list)):
-    num = 0
-    if num_list[i] >= 10:
+def base_to_dec(base_num):
+    result = 0
+    for i in range(0, len(base_num)):
+        ind = (i+1)*-1
+        multiplier = base**i
+        num = base_num[ind] * multiplier
+        result = num + result
+    return result
+
+
+#dec-to-base
+if mode == 0:
+    #getting decimal num
+    decimal_num = None
+    while not type(decimal_num) == int:
+        decimal_num = input("Enter decimal num:\t")
+        try:
+            decimal_num = int(decimal_num)
+        except:
+            decimal_num = None
+    num_list = dec_to_base(decimal_num)
+    result = ""
+    for i in range(len(num_list)):
+        num = 0
+        if num_list[i] >= 10:
             for x in range(10, 16):
                 if num_list[i] == x:
                     num = LETTERS[x-10]
-    else:
-        num = num_list[i]
-    result = str(num) + result
+        else:
+            num = num_list[i]
+        result = str(num) + result
+        print(result)
 
-print(result)
+#base-to-dec
+if mode == 1:
+    #getting base num
+    base_num = input("Enter base num:\t")
+    edited_base = []
+    for i in range(0, len(base_num)):
+        edited_base.append(base_num[i])
+    base_num = edited_base
+    for i in range(0, len(base_num)): 
+        if base_num[i] in LETTERS:
+            for x in range(0, 6):
+                if base_num[i] == LETTERS[x]:
+                    base_num[i] = x+10
+        base_num[i] = int(base_num[i])
+    print(base_to_dec(base_num))
+
